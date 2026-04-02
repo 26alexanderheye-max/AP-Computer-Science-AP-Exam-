d04ad60ad55c42560028eec2ac7ce85bf7c2b959
26alexanderheye-max <26.alexander.heye@carolinaday.org>
2026-04-02 16:47:48 +0000

    First change

---
+++ Computer Science.py/My AP Computer Science Final/My Ap Computer Science Project.py
@@ -0,0 +1,35 @@
+
+def introduction():
+    print("Hello user! Please enter your name.")
+    playername = input()
+    print(f"Hello {playername}, you were on a plane when the pilot fell asleep and your plane crashed into a lake in a rural Mexican jungle.")
+    print(f"You were the only survivor of the crash and are in dier need of help. In order to survive you need to take immediate action.")
+    print("The jungle that you landed in is extremely dangerous so you will have to be very smart with the choices you make.")
+
+introduction()
+
+def backpack ()
+    backpack = {}
+    append
+
+def Decision1():
+    print("You can either stay on the plane and try find useful items for the jungle or you can swim to the shore.")
+    print("Type stay to stay on the plane or type swim to swim to shore.")
+    decision1 = input()
+    if decision1 == "stay":
+        print("You decided to stay and search the plane for items.")
+        print("You find a flare gun and someone's backpack containing a water bottle.")
+        print("You decide to take the backpack with the water bottle, do you also want to take the flare gun?")
+        decision2= input()
+        if decision2 == "Yes"
+            append flare gun to backpack
+        elif decision2 == "No"
+
+    elif decision1 == "swim":
+        print("")
+
+
+
+
+
+    
