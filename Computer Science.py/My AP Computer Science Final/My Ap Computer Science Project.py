ff0577aeed9cdb7d68e2b1c923b421716fd6643e
26alexanderheye-max <26.alexander.heye@carolinaday.org>
2026-04-03 19:42:59 +0000

    New changes

--- Computer Science.py/My AP Computer Science Final/My Ap Computer Science Project.py
+++ Computer Science.py/My AP Computer Science Final/My Ap Computer Science Project.py
@@ -49,6 +49,53 @@ def Decision2():
             Decision2()
 
 Decision2 ()
+
+def Decision3 ():
+    print("You need to find food quickly, would you like to stay at the shore, and look food that washed up" \
+    "Or would you like to go into the jungle, and search for food in there?")
+    decision3 = input("Type stay or jungle: ")
+    if decision3 == "stay":
+        print ("You decided to stay at the shore and you find a machete, and a bag of chips")
+        print ("Would you like to pick up the machete, the bag of chips, or both?")
+        decision4 = input ()
+        if decision4 == "the machete":
+            backpack.append ("machete")
+        elif decision4 == "The bag of chips":
+            backpack.append ("Bag of chips")
+        elif decision4 == "both":
+            backpack.append ("Machete")
+            backpack.append ("The back of chips")
+
+        print("Would you now like to go to the jungle")
+        if decision4 == "Yes":
+                Jungle ()
+        elif decision4 == "no":
+            print ("You made camp on the shore and slept and now you explore the jungle") 
+Decision3 ()
+
+def Decision4 ():
+    print("You walk through the jungle and find a hidden Mayan temple")
+    print("You can either climb up to the top of it, or enter through a doorway in the side.")
+    print("Type either climb or enter")
+    Mayantempledecision = input ()
+    if Mayantempledecision == "climb":
+        print ("You chose to climb to the top of the temple")
+        print ("When you climb to the top you see a helicopter flying in the distance looking for surviors of the nearby crash")
+        print ("Due to your elevation shooting a flare gun from this height would get their attention.")
+    elif
+        print("You walk down a narrow passageway and trip a wire causing the floor to open and you to fall down a hole to your death")
+        print("Game Over. Type restart to play again.")
+        answer = input() 
+        if answer == "restart":
+            Decision1 ()
+            Decision2 ()
+            Decision3 ()
+            Decision4 ()
+
+
+
+
+
     
 
 
