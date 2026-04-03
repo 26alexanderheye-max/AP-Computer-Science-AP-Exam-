a53f6cddc2da951600c7e0138943666f2a6cff5a
26alexanderheye-max <26.alexander.heye@carolinaday.org>
2026-04-03 04:09:18 +0000

    Goated Project

--- Computer Science.py/My AP Computer Science Final/My Ap Computer Science Project.py
+++ Computer Science.py/My AP Computer Science Final/My Ap Computer Science Project.py
@@ -3,14 +3,12 @@ def introduction():
     print("Hello user! Please enter your name.")
     playername = input()
     print(f"Hello {playername}, you were on a plane when the pilot fell asleep and your plane crashed into a lake in a rural Mexican jungle.")
-    print(f"You were the only survivor of the crash and are in dier need of help. In order to survive you need to take immediate action.")
+    print(f"You were the only survivor of the crash and all you have is your empty backpack. In order to survive you need to take immediate action.")
     print("The jungle that you landed in is extremely dangerous so you will have to be very smart with the choices you make.")
 
 introduction()
 
-def backpack ()
-    backpack = {}
-    append
+backpack = [] 
 
 def Decision1():
     print("You can either stay on the plane and try find useful items for the jungle or you can swim to the shore.")
@@ -18,15 +16,46 @@ def Decision1():
     decision1 = input()
     if decision1 == "stay":
         print("You decided to stay and search the plane for items.")
-        print("You find a flare gun and someone's backpack containing a water bottle.")
-        print("You decide to take the backpack with the water bottle, do you also want to take the flare gun?")
+        print("You found a flare gun and a water bottle.")
+        print("You decide to take the the water bottle, do you also want to take the flare gun?")
         decision2= input()
-        if decision2 == "Yes"
-            append flare gun to backpack
-        elif decision2 == "No"
+        if decision2 == "Yes":
+            backpack.append("flare gun")
+            backpack.append("water bottle")
+            print("You decided to take both the flare gun and the water bottle.")
+        elif decision2 == "No":
+            backpack.append("water bottle")
+            print("You only took the water bottle.")
+            print("Now you start your swim to shore.")
 
     elif decision1 == "swim":
-        print("")
+        print("You decided to swim to shore.")
+        
+Decision1()
+
+
+def Decision2():
+    print("You are extremely thirsty after the swim to shore, do you want to drink then water in your water bottle?")
+    decision2 = input("Type yes or no): ")
+    if decision2 == "yes":
+        print ("You decided to drink the water and feel refreshed.")
+    elif decision2 == "no":
+        print("You decided not to drink the water and die of thirst.")
+        print("Game Over. Type restart to play again.")
+        answer = input() 
+        if answer == "restart":
+            introduction()
+            Decision1() 
+            Decision2()
+
+Decision2 ()
+    
+
+
+
+
+
+
 
 
 
